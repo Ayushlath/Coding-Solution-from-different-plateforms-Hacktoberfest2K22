a45805ae65a83bcb9907dc7fff61f37e64318b7d
akashprap <akashprap2002@gmail.com>
2022-10-03 09:54:50 +0530

    Added Knapsack

---
+++ Python/knapsack.py
@@ -0,0 +1,14 @@
+def knapsack(wt,val,W,N):
+    if N==0 or W==0:
+        return 0
+    elif wt[N-1]<=W:
+        return max((val[N-1]+knapsack(wt,val,W-wt[N-1],N-1)),knapsack(wt,val,W,N-1))
+    elif (wt[N-1]>W):
+        return knapsack(wt,val,W,N-1)
+#__main()__:
+wt=list(map(int,input().split()))
+val=list(map(int,input().split()))
+W=int(input())
+N=len(wt)
+a=knapsack(wt,val,W,N)
+print(a)
